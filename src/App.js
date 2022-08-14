05f2374d7be3723c29e0fdd34ffcb5a22f125003
BogdanGorelkin <b.gorelkin@yandex.ru>
2022-08-14 18:43:54 +0200

    empty app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+// import { Link } from "react-router-dom";
+import About from "./components/about";
+
+function App() {
+  return (
+  <div>
+  {/* <Link to="/about">About</Link> */}
+  <h2>Github Pages</h2>
+  <h3>Deploying React to Github</h3>
+  </div>
+  );
+ }
+ export default App;
